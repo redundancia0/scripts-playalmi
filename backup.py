5d43cc93fb1e73281515bfa8e48801e4f1cd61fa
redundancia0 <149798201+redundancia0@users.noreply.github.com>
2024-04-24 08:39:11 +0200

    Subiendo el script que gestiona los backups
    Comprime la carpeta home y se comunica con la API

---
+++ backup.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+import requests
+
+ruta = "localhost:8080"
+
+def compress_folder(folder_path, zip_path):
+    shutil.make_archive(zip_path, 'zip', folder_path)
+
+def send_backup(zip_file):
+    api_url = f"http://{ruta}/api/backup/receive"
+
+    try:
+        with open(zip_file, 'rb') as file:
+            files = {'file': file}
+            response = requests.post(api_url, files=files)
+            if response.status_code == 200:
+                print("Backup file uploaded successfully.")
+            else:
+                print(f"Failed to upload backup file. Status code: {response.status_code}")
+    except FileNotFoundError:
+        print("Backup file not found.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+if __name__ == "__main__":
+    folder_to_compress = "/home/"
+    zip_file = "/tmp/home_backup"
+    compress_folder(folder_to_compress, zip_file)
+    zip_file += ".zip"
+    send_backup(zip_file)
